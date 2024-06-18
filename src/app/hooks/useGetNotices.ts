946dace5901923777741820894ec886a200efd8f
koborikan <koborikan@koborikannoMacBook-Pro.local>
2024-06-19 06:47:38 +0900

    通知回答機能

--- src/app/hooks/useGetNotices.ts
+++ src/app/hooks/useGetNotices.ts
@@ -3,11 +3,12 @@ import axios from 'axios';
 
 export function useGetNotices() {
   const fetcher = async () => {
-    const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/notice/getNotices`);
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/notice/getNotices`
+    );
     return res;
   };
   const { data, error, isLoading, mutate } = useSWR<any>(`/notice`, fetcher);
-
   // 呼び出し元で命名した方が、いい
   return {
     notices: data,
--- src/app/notice/page.tsx
+++ src/app/notice/page.tsx
@@ -3,14 +3,43 @@
 import React from 'react';
 import { useGetNotices } from '@/app/hooks/useGetNotices';
 import axios from 'axios';
+import { format } from 'date-fns';
 
 const NoticePage = () => {
   const { notices, isError, isLoading: isNoticesLoading } = useGetNotices();
+  const addAcceptedId = async (userId: number, jobId: number) => {
+    try {
+      await axios.patch(
+        `${process.env.NEXT_PUBLIC_API_URL}/job/addAcceptedId`,
+        {
+          acceptedId: userId,
+          jobId: jobId,
+        }
+      );
+    } catch (error) {
+      console.error('エラー:', error);
+    }
+  };
+
+  const addRejectedId = async (userId: number, jobId: number) => {
+    try {
+      await axios.patch(
+        `${process.env.NEXT_PUBLIC_API_URL}/job/addRejectedId`,
+        {
+          rejectedId: userId,
+          jobId: jobId,
+        }
+      );
+    } catch (error) {
+      console.error('エラー:', error);
+    }
+  };
+
   return (
     <>
       <main className="flex min-h-screen flex-col items-center px-4">
         {notices && notices.data.length > 0 ? (
-          notices.data.map((job: any, index: number) => (
+          notices.data.map((notice: any, index: number) => (
             <div
               className="shadow-lg rounded-lg bg-white mx-auto p-4 notification-box max-w-md mt-8"
               key={index}
@@ -19,15 +48,27 @@ const NoticePage = () => {
                 <h1 className="text-xl font-bold text-center mb-4">お知らせ</h1>
               </div>
               <p className="text-md mb-6 font-medium">
-                5月10日に東京競馬場で並ぶことができるユーザーが見つかりました。
+                {format(notice.job.startDate, 'M月d日')}に{notice.job.location}
+                で並ぶことができるユーザー({notice.applicant.nickName}
+                さん)が見つかりました。
                 このユーザーに依頼する場合は「はい」を選択、
                 依頼しない場合は「いいえ」を選択してください。
               </p>
               <div className="flex justify-center space-x-4">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded border-blue-700">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded border-blue-700"
+                  onClick={() =>
+                    addAcceptedId(notice.applicant.id, notice.job.id)
+                  }
+                >
                   はい
                 </button>
-                <button className="bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-6 rounded">
+                <button
+                  className="bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-6 rounded"
+                  onClick={() =>
+                    addRejectedId(notice.applicant.id, notice.job.id)
+                  }
+                >
                   いいえ
                 </button>
               </div>
